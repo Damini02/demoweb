3a63fd9badbcc4ffa18720730f5c23bff90860fd
Damini Bisen <daminibisen8@gmail.com>
2021-03-13 09:10:09 +0530

    done

---
+++ action_page.php
@@ -0,0 +1,7 @@
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+
+Email: <input type="text" name="email" value="<?php echo $email;?>">
+
+
+Subject: <textarea name="subject" rows="5" cols="40"><?php echo $subject;?></textarea>
+
